b0e758d91fe3df12ae58e33d37a22efbd84ff183
Katie Morris <katiemorris212@hotmail.com>
2016-11-15 11:28:38 +0000

    converted listvew to gridview on main room list screen

--- RoomCheck/MyRoomsActivity.cs
+++ RoomCheck/MyRoomsActivity.cs
@@ -18,7 +18,7 @@ namespace RoomCheck
     public class MyRoomsActivity : Activity
     {
         List<Room> rooms = new List<Room>();
-        ListView listView;
+        GridView gridview;
         DBRepository dbr = new DBRepository();
 
 
@@ -29,21 +29,22 @@ namespace RoomCheck
             // Create your application here
             SetContentView(Resource.Layout.MyRoomsMain);
 
-            listView = FindViewById<ListView>(Resource.Id.lstRooms);
+            //TODO: change to gridview
+            gridview = FindViewById<GridView>(Resource.Id.lstRooms);
 
             rooms = dbr.GetAllRooms();
         
-            listView.Adapter = new RoomAdapter(this, rooms);
+            gridview.Adapter = new RoomAdapter(this, rooms);
 
-            listView.ItemClick += ListViewOnItemClick;
+            gridview.ItemClick += ListViewOnItemClick;
 
         }
 
         private void ListViewOnItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
-            if (listView.GetItemAtPosition(e.Position) != null)
+            if (gridview.GetItemAtPosition(e.Position) != null)
             {
-                var room = listView.GetItemAtPosition(e.Position).Cast<Room>();
+                var room = gridview.GetItemAtPosition(e.Position).Cast<Room>();
 
                 //pass the room id to the next activity
                 var roomDetailsActivity = new Intent(this, typeof(RoomDetailsActivity));
@@ -52,7 +53,7 @@ namespace RoomCheck
             }
             else
             {
-                string item = listView.GetItemAtPosition(e.Position).ToString();
+                string item = gridview.GetItemAtPosition(e.Position).ToString();
                 Toast.MakeText(this, item, ToastLength.Short).Show();
             }
         }
@@ -88,26 +89,26 @@ namespace RoomCheck
         {
             var item = items[position];
 
-            View view = convertView;
-            if (view == null) // no view to re-use, create new
-                view = context.LayoutInflater.Inflate(Resource.Layout.MyRooms, null);
+            View view = convertView ?? context.LayoutInflater.Inflate(Resource.Layout.MyRooms, null);
+
+            //TODO: see how often this method is getting called and if there are any changes i can make to optimise
 
             RoomOccupiedStatus roomOccStatus = dbr.GetOccupiedStatusById(item.OccupiedStatusID);
             RoomCleanStatus roomCleanStatus = dbr.GetCleanStatusById(item.CleanStatusID);
             RoomType roomType = dbr.GetRoomTypeById(item.RoomTypeID);
 
+            //TODO: if room type is empty grey out the icon and border (room occupied status shoud always be unoccupied on empty rooms
             ImageView imgRoomIcon = view.FindViewById<ImageView>(Resource.Id.imgRoomIcon);
 
             //populate room number labels
             view.FindViewById<TextView>(Resource.Id.lblRoomNo).Text = item.RoomNo;
 
             //change icon to reflect occupied status
-            int roomOccResourceId = (int)typeof(Resource.Drawable).GetField(roomOccStatus.IconPath).GetValue(null);
+            var roomOccResourceId = (int)typeof(Resource.Drawable).GetField(roomOccStatus.IconPath).GetValue(null);
             imgRoomIcon.SetImageResource(roomOccResourceId);
 
-
             //change border colour to reflect cleaning status
-            int background = (int)typeof(Resource.Drawable).GetField(roomCleanStatus.BorderImage).GetValue(null);
+            var background = (int)typeof(Resource.Drawable).GetField(roomCleanStatus.BorderImage).GetValue(null);
             view.FindViewById<ImageView>(Resource.Id.imgRoomIcon).SetBackgroundResource(background);
 
             return view;
